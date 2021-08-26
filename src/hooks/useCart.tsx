4e1f9aa9d2e97f46e52cfe8f5c094a881bb8295e
José Oliveira <j.oliveira90@live.com>
2021-08-26 12:24:16 -0300

    return cartSize

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,7 +1,8 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
-import { toast } from 'react-toastify';
-import { api } from '../services/api';
-import { Product, Stock } from '../types';
+import { useEffect } from "react";
+import { createContext, ReactNode, useContext, useState } from "react";
+import { toast } from "react-toastify";
+import { api } from "../services/api";
+import { Product, Stock } from "../types";
 
 interface CartProviderProps {
   children: ReactNode;
@@ -23,20 +24,27 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem("@RocketShoes:cart");
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
+  // useEffect(() => {
+  //   localStorage.setItem("@RocketShoes:cart", JSON.stringify(cart));
+  // }, [cart]);
+
   const addProduct = async (productId: number) => {
     try {
-      // TODO
-    } catch {
-      // TODO
+      await api
+        .get(`/products/${productId}`)
+        .then((response) => setCart([...cart, response.data]));
+    } catch (error) {
+      alert(error);
+    } finally {
     }
   };
 
@@ -53,7 +61,20 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-      // TODO
+      const response = await api.get(`/products/${productId}`);
+
+      const { data } = response;
+
+      setCart([
+        ...cart,
+        {
+          id: data.id,
+          title: data.title,
+          price: data.price,
+          image: data.image,
+          amount: amount,
+        },
+      ]);
     } catch {
       // TODO
     }
